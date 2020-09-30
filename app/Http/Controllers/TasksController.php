d48faec6d6ecd842f792281b2b936bff613c1686
atky <sno.fnsd.2010@gmail.com>
2020-09-30 10:18:57 +0000

    test

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -19,13 +19,18 @@ class TasksController extends Controller
         if (\Auth::check()) { // 認証済みの場合
             // 認証済みユーザを取得
             $user = \Auth::user();
-        
-        $tasks = Task::all();
+        $tasks = $user->tasks()->orderBy('created_at', 'desc')->paginate(10);
+
+            $data = [
+                'user' => $user,
+                'tasks' => $tasks,
+            ];
+        }
         
         return view('tasks.index', [
             'tasks'=>$tasks,
         ]);
-      }
+      
       
        return view('welcome');
     }
